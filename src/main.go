bb011e61565f94fe44e464dbadb60ec6b0622586
Petar Petrov <pppepito86@gmail.com>
2017-01-11 11:25:33 +0000

    add loan details

--- src/main.go
+++ src/main.go
@@ -27,6 +27,14 @@ type Result struct {
   InterestPayment float64 `json:"interestPayment"`
 }
 
+type DetailedResult struct {
+  Month int `json:"month"`
+  TotalPrincipalBefore float64 `json:"totalPrincipalBefore"`
+  Interest float64 `json:"interest"`
+  PrincipalPayment float64 `json:"principalPayment"`
+  MonthPrincipal float64 `json:"monthPayment"`
+}
+
 //loan/ammount/30000/term/30/interest/6.5
 func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -38,16 +46,46 @@ func handler(w http.ResponseWriter, r *http.Request) {
           return
         }
 	ammount, _ := strconv.ParseFloat(splitURL[3], 64)
-	term, _ := strconv.ParseFloat(splitURL[5], 64)
+        term, _ := strconv.Atoi(splitURL[5])
 	interest, _ := strconv.ParseFloat(splitURL[7], 64)
+        var json []byte
+        if len(splitURL) == 9 {
+          json = details(ammount, term, interest)
+        } else {
+          json = result(ammount, term, interest)
+        }
+	w.Write(json)
+}
 
+func result(ammount float64, term int, interest float64) []byte {
 	rr := interest/1200
-        A := (math.Pow(1+rr, term)-1)/(rr*math.Pow(1+rr, term))
+        A := (math.Pow(1+rr, float64(term))-1)/(rr*math.Pow(1+rr, float64(term)))
         Pv := ammount/A
-	loan := Result{round(Pv), round(Pv*term), round(Pv*term-ammount)}
+	loan := Result{round(Pv), round(Pv*float64(term)), round(Pv*float64(term)-ammount)}
 
 	json, _ := json.Marshal(loan)
-	w.Write(json)
+        return json
+}
+
+func details(ammount float64, term int, interest float64) []byte {
+  r := interest/1200
+  A := (math.Pow(1+r, float64(term))-1)/(r*math.Pow(1+r, float64(term)))
+  M := ammount/A
+  principal := 30000.0
+  drs := make([]DetailedResult, term)
+  for i := 1; i <= term; i++ {
+    principalPayment := principal*r
+    monthPayment := 0.0
+    if principal > 0 {
+      monthPayment = M - principalPayment 
+    }
+    dr:= DetailedResult{i, principal, principalPayment, monthPayment, M}
+    drs[i-1] = dr
+
+    principal -= monthPayment
+  }
+  json, _ := json.Marshal(drs)
+  return json
 }
 
 func round(x float64) float64 {
