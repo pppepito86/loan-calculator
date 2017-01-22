f5725584dbb6e6d33bc32e4f908537aa59ee010f
Petar Petrov <pppepito86@gmail.com>
2017-01-22 22:31:35 +0000

    fix small columns and rest url

--- src/app/page/http.service.ts
+++ src/app/page/http.service.ts
@@ -8,12 +8,12 @@ export class HttpService {
   constructor(private http: Http) { }
 
   getData(ammount: string, term: string, interest: string) {
-    return this.http.get("http://localhost:3005/loan/ammount/"+ammount+"/term/"+term+"/interest/"+interest)
+    return this.http.get("/loan/ammount/"+ammount+"/term/"+term+"/interest/"+interest)
       .map((response: Response) => response.json());
   }
 
   getDetails(ammount: string, term: string, interest: string) {
-    return this.http.get("http://localhost:3005/loan/ammount/"+ammount+"/term/"+term+"/interest/"+interest+"/details")
+    return this.http.get("/loan/ammount/"+ammount+"/term/"+term+"/interest/"+interest+"/details")
       .map((response: Response) => response.json());
   }
 
