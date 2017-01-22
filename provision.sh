71752de83283aa518552dbb97e82a4bc7711c7c4
Petar Petrov <pppepito86@gmail.com>
2017-01-22 22:59:03 +0000

    angular in provision.sh

--- provision.sh
+++ provision.sh
@@ -13,6 +13,9 @@ curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
 
+# angular
+npm install -g angular-cli
+
 #clone app
 git clone https://github.com/pppepito86/loan_calculator.git /app/judge
 
